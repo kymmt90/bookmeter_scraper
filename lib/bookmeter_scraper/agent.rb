d8b8b0a24429dda0fbb6e6ea0d94bb71f1f2da8e
yutaszk <yutaszk23@gmail.com>
2017-10-04 14:48:02 +0900

    Fix login form name

--- lib/bookmeter_scraper/agent.rb
+++ lib/bookmeter_scraper/agent.rb
@@ -24,8 +24,8 @@ module BookmeterScraper
       page_after_submitting_form = nil
       @agent.get(BookmeterScraper::LOGIN_URI) do |page|
         page_after_submitting_form = page.form_with(action: '/login') do |form|
-          form.field_with(name: 'mail').value     = config.mail
-          form.field_with(name: 'password').value = config.password
+          form.field_with(name: 'session[email_address]').value     = config.mail
+          form.field_with(name: 'session[password]').value = config.password
         end.submit
       end
 
